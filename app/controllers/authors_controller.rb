eec1ad79bfe7a021596698bee905422ad3b512eb
jomariepolanco <jomariepolanco@gmail.com>
2020-10-29 20:18:29 -0400

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,44 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    @author = find_author 
+    render :show 
   end
 
   def new
     @author = Author.new
+    render :new 
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new 
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    @author = find_author
+  end
+
+  def update
+    @author = find_author
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end 
   end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def find_author
+    @author = Author.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors
+  post '/authors/new', to: 'authors#create'
+  patch '/authors/:id/edit', to: 'authors#update'
+  
+  resources :posts
+  post '/posts/new', to: 'posts#create'
+  patch '/posts/:id/edit', to: 'posts#update'
+
 end
